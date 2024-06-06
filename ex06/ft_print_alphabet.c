fef4beec04bf65ec37667826d69ca2dfab408d0e
Javier Alvarez Villa <jotastratego@gmail.com>
2024-06-07 01:16:26 +0200

    Ex06

--- ex06/ft_print_alphabet.c
+++ ex06/ft_print_alphabet.c
@@ -6,10 +6,9 @@
 /*   By: javialva <javialva@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/06/07 01:08:25 by javialva          #+#    #+#             */
-/*   Updated: 2024/06/07 01:12:35 by javialva         ###   ########.fr       */
+/*   Updated: 2024/06/07 01:15:59 by javialva         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
-
 #include <unistd.h>
 
 void	ft_putchar(char c)
@@ -17,12 +16,12 @@ void	ft_putchar(char c)
 	write(1, &c, 1);
 }
 
-void	ft_print_alphabet()
+void	ft_print_alphabet(void)
 {
 	char	a;
 
 	a = 'a';
-	while(a <= 'z')
+	while (a <= 'z')
 	{
 		ft_putchar(a);
 		a++;
