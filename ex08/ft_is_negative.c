1eedddc9a31a240304e57fd8ffb0f70df8d4c6cf
Javier Alvarez Villa <jotastratego@gmail.com>
2024-06-07 01:28:50 +0200

    Ex08

---
+++ ex08/ft_is_negative.c
@@ -0,0 +1,31 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_is_negative.c                                   :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: javialva <javialva@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/06/07 01:21:47 by javialva          #+#    #+#             */
+/*   Updated: 2024/06/07 01:28:32 by javialva         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include <unistd.h>
+
+void	ft_putchar(char c)
+{
+	write(1, &c, 1);
+}
+
+void	ft_is_negative(int n)
+{
+	char	neg;
+	char	pos;
+
+	neg = 'N';
+	pos = 'P';
+	if (n < 0)
+		ft_putchar(neg);
+	else
+		ft_putchar(pos);
+}
