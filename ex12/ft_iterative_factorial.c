bdf39b02620855c3daa55f23812d2f33a6c07f2c
Javier Álvarez Villa <jotastratego@gmail.com>
2024-06-07 17:25:05 +0200

    Ex12

---
+++ ex12/ft_iterative_factorial.c
@@ -0,0 +1,27 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_iterative_factorial.c                           :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: javialva <javialva@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/06/07 16:32:06 by javialva          #+#    #+#             */
+/*   Updated: 2024/06/07 17:24:52 by javialva         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+int	ft_iterative_factorial(int nb)
+{
+	int	result;
+
+	result = 1;
+	if (nb < 0)
+		return (0);
+	else if (nb == 0)
+		return (1);
+	while (nb > 0)
+	{
+		result *= nb;
+		nb--;
+	}
+	return (result);
+}
