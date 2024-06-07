28303861740ef9215959157b9ef902033c9f4e53
Javier Álvarez Villa <jotastratego@gmail.com>
2024-06-07 17:43:09 +0200

    Ex13

---
+++ ex13/ft_recursive_factorial.c
@@ -0,0 +1,21 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_recursive_factorial.c                           :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: javialva <javialva@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/06/07 17:26:19 by javialva          #+#    #+#             */
+/*   Updated: 2024/06/07 17:42:42 by javialva         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+int	ft_recursive_factorial(int nb)
+{
+	if (nb < 0)
+		return (0);
+	else if (nb == 0)
+		return (1);
+	else
+		return (nb * ft_recursive_factorial(nb - 1));
+}
