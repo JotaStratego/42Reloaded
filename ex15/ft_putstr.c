ac567397971df74ad9875da8e82e1226a256a4bb
Javier Alvarez Villa <javialva@cbr6s2.42barcelona.com>
2024-06-08 11:54:27 +0200

    ex15

--- ex15/ft_putstr.c
+++ ex15/ft_putstr.c
@@ -6,15 +6,10 @@
 /*   By: javialva <javialva@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/06/07 18:23:48 by javialva          #+#    #+#             */
-/*   Updated: 2024/06/07 18:29:50 by javialva         ###   ########.fr       */
+/*   Updated: 2024/06/08 11:32:50 by javialva         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 #include <unistd.h>
-
-void	ft_putchar(char c)
-{
-	write(1, &c, 1);
-}
 
 void	ft_putstr(char *str)
 {
