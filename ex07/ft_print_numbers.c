0803d9172c3e00276fd6f134f343ddc31e30f2ef
Javier Alvarez Villa <javialva@cbr6s2.42barcelona.com>
2024-06-08 11:52:43 +0200

    ex07

--- ex07/ft_print_numbers.c
+++ ex07/ft_print_numbers.c
@@ -6,16 +6,11 @@
 /*   By: javialva <javialva@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/06/07 01:17:38 by javialva          #+#    #+#             */
-/*   Updated: 2024/06/07 01:20:57 by javialva         ###   ########.fr       */
+/*   Updated: 2024/06/08 11:23:40 by javialva         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include <unistd.h>
-
-void	ft_putchar(char c)
-{
-	write(1, &c, 1);
-}
 
 void	ft_print_numbers(void)
 {
