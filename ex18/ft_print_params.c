1eb33526e25fbedbccc8d034ef21bde246d1d8cc
Javier Álvarez Villa <jotastratego@gmail.com>
2024-06-09 19:54:34 +0200

    ex18

---
+++ ex18/ft_print_params.c
@@ -0,0 +1,32 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_print_params.c                                  :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: javialva <javialva@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/06/09 19:29:49 by javialva          #+#    #+#             */
+/*   Updated: 2024/06/09 19:54:17 by javialva         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+#include <unistd.h>
+
+void	ft_putchar(char c);
+
+int	main(int argc, char **argv)
+{
+	int	i;
+
+	i = 1;
+	while (i < argc)
+	{
+		while (*argv[i] != '\0')
+		{
+			ft_putchar(*argv[i]);
+			argv[i]++;
+		}
+		ft_putchar('\n');
+		i++;
+	}
+	return (0);
+}
