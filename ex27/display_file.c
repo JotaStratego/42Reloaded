285acc1f3c84d31af6479bb7dbb84ac162baae7d
Javier Álvarez Villa <javieralvarezvilla@gmail.com>
2024-06-12 20:57:34 +0200

    ex27

--- ex27/display_file.c
+++ ex27/display_file.c
@@ -6,15 +6,41 @@
 /*   By: javialva <javialva@student.42barcelon      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/06/12 12:44:22 by javialva          #+#    #+#             */
-/*   Updated: 2024/06/12 12:46:42 by javialva         ###   ########.fr       */
+/*   Updated: 2024/06/12 20:51:24 by javialva         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
-void	ft_putchar(char c)
+#include <unistd.h>
+#include <fcntl.h>
+#define BUF_SIZE 1024
+
+void	print_file(char const *name)
 {
-	write(1, &c, 1);
+	int		fd;
+	int		bytes_read;
+	char	buffer[BUF_SIZE + 1];
+
+	fd = open(name, O_RDONLY);
+	if (fd == -1)
+	{
+		write(2, "Cannot read file.\n", 18);
+		return ;
+	}
+	bytes_read = read(fd, buffer, BUF_SIZE);
+	while (bytes_read > 0)
+	{
+		write(1, buffer, bytes_read);
+		bytes_read = read(fd, buffer, BUF_SIZE);
+	}
+	close(fd);
 }
 
-int	main(int argc, char **arg)
+int	main(int argc, char **argv)
 {
-	
+	if (argc == 2)
+		print_file(argv[1]);
+	else if (argc < 2)
+		write(2, "File name missing.\n", 19);
+	else
+		write(2, "Too many arguments.\n", 20);
+	return (0);
 }
